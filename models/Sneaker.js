3b212e6457c343295ff2386754fea5a6f6ad7931
Maxime Guichard <maxime_guichard@hotmail.fr>
2020-05-22 18:03:38 +0200

    Modif models

---
+++ models/Sneaker.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const sneakerSchema = new Schema({
+    name: String,  
+    ref: String,  
+    size: Number,  
+    description: String,  
+    price: Number,  
+    category: String,  
+    id_tags: Number
+})
+
+const sneakerModel = mongoose.model("Sneaker", sneakerSchema);
+module.exports = sneakerModel;
